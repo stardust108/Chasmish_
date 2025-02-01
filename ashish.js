d2ff435f7dd4c4f2b2f22cfbd9f37eac1b58cc8a
stardust108 <nishsingh446@gmail.com>
2025-02-01 20:48:42 +0530

    Update ashish.js

--- ashish.js
+++ ashish.js
@@ -1,9 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let pages = document.querySelectorAll(".page");
-  
-  pages.forEach((page, index) => {
-    page.addEventListener("click", function () {
-      this.classList.toggle("flipped");
-    });
-  });
-});
+let currentPage = 1;
+
+function toggleClass(e, toggleClassName) {
+  if (e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(" " + toggleClassName, "");
+  } else {
+    e.className += " " + toggleClassName;
+  }
+}
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage += 2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+  } else if ((page = currentPage - 1)) {
+    currentPage -= 2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+  }
+}
